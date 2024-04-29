29bdf211996b745ee6d77bad6acc804a386f7bbf
annask94 <annasobieraj3@gmail.com>
2024-04-30 00:23:40 +0200

    Fix: Error catch

--- app/estimate/actions.ts
+++ app/estimate/actions.ts
@@ -1,6 +1,5 @@
 "use server";
 import OpenAI from "openai";
-import { redirect } from "next/navigation";
 
 type FormState = {
   userCard: string;
@@ -10,7 +9,7 @@ type FormState = {
 
 export async function handlePrompt(prevState: FormState, formData: FormData) {
   const prompt = formData.get("prompt");
-  const pickedCard = formData.get("cardSelection") || "";
+  const pickedCard = (formData.get("cardSelection") as string) || "";
 
   console.log(prompt);
   console.log(pickedCard);
@@ -20,29 +19,36 @@ export async function handlePrompt(prevState: FormState, formData: FormData) {
     apiKey: process.env.OPENAI_KEY,
   });
 
-  const completion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    max_tokens: 150,
-    temperature: 0.5,
-    stream: false,
-    messages: [
-      {
-        role: "user",
-        content: `Based on the provided task description '${prompt}', please estimate the effort required using the Scrum poker technique. Consider factors such as the complexity of implementation, dependencies, and potential risks. Return an estimate as a JSON in a structured format: { "aiEstimate": "5", "aiDescription": "Detailed explanation here..." }`,
-      },
-    ],
-  });
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      max_tokens: 150,
+      temperature: 0.5,
+      stream: false,
+      messages: [
+        {
+          role: "user",
+          content: `Based on the provided task description '${prompt}', please estimate the effort required using the Scrum poker technique. Consider factors such as the complexity of implementation, dependencies, and potential risks. Return an estimate as a JSON in a structured format: { "aiEstimate": "5", "aiDescription": "Detailed explanation here..." }`,
+        },
+      ],
+    });
 
-  if (completion.choices.length > 0 && completion.choices[0].message.content) {
-    const aiResponseData = JSON.parse(completion.choices[0].message.content);
-    console.log(aiResponseData);
-    return {
-      userCard: JSON.stringify(pickedCard),
-      aiCard: aiResponseData.aiEstimate,
-      aiDescription: aiResponseData.aiDescription,
-    };
-  } else {
-    alert("Sorry, something went wrong. Please try again");
+    if (
+      completion.choices.length > 0 &&
+      completion.choices[0].message.content
+    ) {
+      const aiResponseData = JSON.parse(completion.choices[0].message.content);
+      return {
+        userCard: pickedCard,
+        aiCard: aiResponseData.aiEstimate,
+        aiDescription: aiResponseData.aiDescription,
+      };
+    } else {
+      console.error("No valid response from AI.");
+      return null; // Consider how to handle errors in your application context
+    }
+  } catch (error) {
+    console.error("Error processing AI completion:", error);
     return null;
   }
 }
