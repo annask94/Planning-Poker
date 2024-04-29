0c95e00b4084ed58b5461cf873793ad50583b242
annask94 <annasobieraj3@gmail.com>
2024-04-30 00:12:25 +0200

    Fix: Display AI results

--- app/estimate/page.tsx
+++ app/estimate/page.tsx
@@ -1,36 +1,74 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import { useFormState } from "react-dom";
 import { handlePrompt } from "./actions";
 import CardSet from "@/components/CardSets";
-import SubmitBtn from "@/components/submitBtn";
+import SubmitBtn from "@/components/SubmitBtn";
 
 export default function Home() {
+  const [showPopup, setShowPopup] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
+
   const [state, action] = useFormState(handlePrompt, {
     userCard: "",
     aiCard: "",
     aiDescription: "",
   });
+
+  useEffect(() => {
+    if (state?.aiCard && state?.aiDescription) {
+      setShowPopup(true);
+    }
+  }, [state?.aiCard, state?.aiDescription]);
+
   return (
-    <form
-      className="flex flex-col gap-8 justify-center items-center"
-      action={action}
-    >
-      <label htmlFor="taskDescription" className="text-xl md:text-4xl">
-        Describe the task
-      </label>
-      <textarea
-        id="taskDescription"
-        name="prompt"
-        rows={5}
-        cols={50}
-        className="task_description rounded-md border-2 border-gray-300 outline-none"
-      />
-      <h2 className="text-xl md:text-4xl">Pick a card</h2>
-      <CardSet />
-      <SubmitBtn />
-      <p>{state.userCard}</p>
-      <p>{state?.aiCard}</p>
-      <p>{state?.aiDescription}</p>
-    </form>
+    <>
+      <form
+        className="flex flex-col gap-8 justify-center items-center"
+        action={action}
+      >
+        <label htmlFor="taskDescription" className="text-xl md:text-4xl">
+          Describe the task
+        </label>
+        <textarea
+          id="taskDescription"
+          name="prompt"
+          rows={5}
+          cols={50}
+          className="task_description rounded-md border-2 border-gray-300 outline-none"
+        />
+        <h2 className="text-xl md:text-4xl">Pick a card</h2>
+        <CardSet />
+        <SubmitBtn />
+      </form>
+
+      {showPopup && (
+        <div className="fixed inset-0 bg-black bg-opacity-40 backdrop-blur-lg z-50 flex justify-center items-center pt-10">
+          <div className="bg-white p-8 rounded-lg shadow-lg max-w-4xl flex flex-col justify-center items-center gap-4">
+            <h2 className="text-2xl font-bold">AI Estimate</h2>
+            <p className="font-semibold">Your Card {state?.userCard}</p>
+            <p className="font-semibold">AI Card {state?.aiCard}</p>
+            <button
+              onClick={() => setShowDetails(!showDetails)}
+              className="text-sm underline hover:opacity-70"
+            >
+              {showDetails ? "Hide Details" : "Show Details"}
+            </button>
+            {showDetails && (
+              <p className="text-justify"> {state?.aiDescription}</p>
+            )}
+
+            <button
+              onClick={() => {
+                window.location.reload();
+              }}
+              className="btn_component text-l md:text-xl px-14 py-1 rounded-md font-medium text-white hover:opacity-70"
+            >
+              Estimate again!
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
